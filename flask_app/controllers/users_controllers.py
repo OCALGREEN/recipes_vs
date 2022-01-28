c94c0595a3cf9807faf13576cf302cd27aee095e
Oger Carrillo <ogercarrillo@gmail.com>
2022-01-27 18:16:55 -0800

    recipes_initial_commit

---
+++ flask_app/controllers/users_controllers.py
@@ -0,0 +1,41 @@
+from flask import render_template, redirect, request, session 
+from flask_app.models.user import User 
+from flask_bcrypt import Bcrypt 
+from flask_app import app
+bcrypt = Bcrypt(app) 
+
+@app.route("/")
+def home():
+    if "uuid" in session: # will send the user to the dashboard if they have signed in and not out yet
+        return redirect("/dashboard")
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not User.register_validator(request.form): # will check to see if all inputs are valid
+        return redirect("/")
+    else: # hashes the password and creates a new user
+        hash_browns = bcrypt.generate_password_hash(request.form["password"]) # bcrypt function to hash the password
+        user_data = {**request.form, "password": hash_browns} # stores the user input and the now hashed password in the varialbe
+        session["uuid"] = User.create(user_data) # adds the user data to session to user late
+        return redirect("/dashboard")
+
+@app.route("/dashboard")
+def success():
+    if "uuid" not in session: # will send the user back to the home page if they are not logged in
+        return redirect("/") 
+    return render_template("dashboard.html", user = User.get_by_id({"id": session["uuid"]})) # renders dashboard and uses the id in session to grab user information
+
+@app.route("/login", methods=["POST"])
+def login():
+    if not User.login_validator(request.form): # checks the email and password entered 
+        return redirect("/")
+    else: 
+        user = User.get_by_email({"emai": request.form["email"]}) # gets the user id and puts in the email
+        session["uuid"] = user.id # puts the id in session
+        return redirect("/dashboard")
+
+@app.route("/logout")
+def logout():
+    session.clear() # will clear session to prevent the user from entering without logging in
+    return redirect("/")
---
+++ flask_app/models/user.py
@@ -0,0 +1,69 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt 
+from flask_app import app
+from flask import flash
+import re # imports compile to check email
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') # email format
+bcrypt = Bcrypt(app)
+
+class User:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.password = data["password"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+    @classmethod # CREATE 
+    def create(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s,  %(email)s, %(password)s,NOW(), NOW());"
+        result = connectToMySQL("recipes_schema").query_db(query, data)
+        return result
+
+    @classmethod # GET ID
+    def get_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL("recipes_schema").query_db(query, data)
+        return cls(result[0])
+    
+    @classmethod # GET EMAIL
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL("recipes_schema").query_db(query, data)
+        if len(result) < 1: # if the email entered is not in the database then return false
+            return False
+        return cls(result[0])
+
+    @staticmethod # REGISTER VALIDATOR
+    def register_validator(post_data):
+        is_valid = True
+        if not EMAIL_REGEX.match(post_data["email"]): # validate the email address
+            flash("Please enter a valid email adress") # if the email does not match the email_regex format
+            is_valid = False
+        if len(post_data["first_name"]) < 3: # validates the first name
+            flash("Please enter a valid first name")
+            is_valid = False 
+        if len(post_data["last_name"]) < 3: # validates the last name
+            flash("Please enter a valid last name")
+            is_valid = False 
+        if len(post_data["password"]) < 6: # validates the password
+            flash("Please enter a password longer than 6 characters")
+            is_valid = False
+        if post_data["password"] != post_data["confirm_password"]: # validates if both passwords entered match
+            flash("Your password and confirm password do not match")
+            is_valid = False
+        return is_valid
+    
+    @staticmethod # LOGIN VALIDATOR
+    def login_validator(post_data):
+        is_valid = True
+        user = User.get_by_email({"email": post_data["email"]}) # gets the email entered
+        if not user: # checks to see if the email entered is good
+            flash("Incorrect email")
+            return False
+        if not bcrypt.check_password_hash(user.password, post_data["password"]): # hashes the password entered and compares it to the hashed password in the database
+            flash("Incorrect password")
+            return False
+        return is_valid
